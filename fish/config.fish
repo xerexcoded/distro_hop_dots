da3557bc7d66a4ef9db06c1dede1ba30bbe6a22b
xerexcoded <arnavbanerjee61@gmail.com>
2021-12-23 01:26:31 +0530

    straight pain in wayland

--- fish/config.fish
+++ fish/config.fish
@@ -4,11 +4,13 @@ end
 
 set fish_greeting ""
 set PATH $HOME/.cargo/bin $PATH
-alias v "lvim"
+alias v "nvim"
 alias ls "lsd"
 alias cat "bat"
-
+alias ssh "kitty +kitten ssh" 
+alias du "dust"
 set PATH $HOME/.emacs.d/bin $PATH
+set -x MOZ_ENABLE_WAYLAND 1
 
 
 
@@ -29,9 +31,8 @@ set PATH $HOME/.emacs.d/bin $PATH
 
 
 
-
-
+zoxide init fish | source
 
 
   starship init fish | source
-set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/xearch/.ghcup/bin $PATH # ghcup-env
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/xeera/.ghcup/bin $PATH # ghcup-env
